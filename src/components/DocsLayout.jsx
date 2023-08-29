d42ec47187945ced48a95772002d234c37377f0a
Desmond Grealy <grealydesmond@gmail.com>
2023-08-29 14:31:19 -0700

    ...

--- src/components/DocsLayout.jsx
+++ src/components/DocsLayout.jsx
@@ -75,9 +75,11 @@ export function DocsLayout({ children, frontmatter: {
         <div className="min-w-0 max-w-2xl flex-auto px-4 py-16 lg:max-w-none lg:pl-8 lg:pr-0 xl:px-16">
           <article>
             <DocsHeader title={title} />
-            {composed_date}
+            
+            <p >{composed_date}</p>
 
-            Desmond Grealy
+            <p className='mb-12'>Desmond Grealy</p>
+
             <Prose>{children}</Prose>
           </article>
           {/* <PrevNextLinks /> */}
